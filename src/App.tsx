f316f755bed13086536adfe0e63370996c08424e
Girish V <girish.v@needl.ai>
2021-03-12 00:12:06 +0530

    drag back to the cards list

--- src/App.tsx
+++ src/App.tsx
@@ -19,16 +19,23 @@ type DropDataType = {
   id: string
 }
 
+type SelectDropType = {
+  containerIndex: number
+  id: string
+}
+
 type DataContextType = {
   bins: BinType[]
   cards: CardsType[]
   onDrop: (item: DropDataType, bucketIndex: number) => void
+  onSelectDrop: (item: SelectDropType) => void
 }
 
 const DataContext = React.createContext<DataContextType>({
   bins: [],
   cards: [],
   onDrop: () => {},
+  onSelectDrop: () => {},
 })
 
 type CardsType = {
@@ -109,57 +116,111 @@ function DataContextProvider(props: any) {
     [cards],
   )
 
+  const onSelectDrop = React.useCallback(
+    ({ containerIndex, id }: SelectDropType) => {
+      const card = buckets[containerIndex].childrens.find(
+        ({ id: cardId }) => cardId === id,
+      )
+      if (card) {
+        setCards(cards =>
+          [...cards, card].sort(({ label: a }, { label: b }) =>
+            a.localeCompare(b),
+          ),
+        )
+        setBuckets(buckets => [
+          ...buckets.slice(0, containerIndex),
+          {
+            ...buckets[containerIndex],
+            childrens: buckets[containerIndex].childrens.filter(
+              ({ id: cardId }) => cardId !== id,
+            ),
+          },
+          ...buckets.slice(containerIndex + 1),
+        ])
+      }
+    },
+    [buckets],
+  )
+
   const values: DataContextType = React.useMemo(
     () => ({
       bins: buckets,
       cards,
       onDrop,
+      onSelectDrop,
     }),
-    [buckets, cards, onDrop],
+    [buckets, cards, onDrop, onSelectDrop],
   )
 
   return <DataContext.Provider value={values} {...props} />
 }
 
+function DropPlaceholderContainer() {
+  return (
+    <div className='border-4 border-dashed border-gray-600 p-6 grid place-content-center'>
+      <h3 className='text-xl font-bold text-gray-700 text-center'>
+        + Drop here
+      </h3>
+    </div>
+  )
+}
+
+function SelectedCard({
+  containerIndex,
+  label,
+  id,
+}: CardsType & { containerIndex: number }) {
+  const [{ isDragging }, dragRef] = useDrag(() => ({
+    type: 'selected_card',
+    item: { id, containerIndex },
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }))
+  return (
+    <li
+      ref={dragRef}
+      className={`p-2 rounded border border-gray-500 ring ring-gray-500 ${
+        isDragging ? 'opacity-0' : 'opacity-100'
+      }`}
+    >
+      <h5 className='text-base font-bold text-gray-800'>{label}</h5>
+    </li>
+  )
+}
+
 function Dropzone({
   accept,
   childrens,
+  index,
   label,
   onDrop,
-}: BinType & { onDrop: (item: DropDataType) => void }) {
-  const [{ canDrop }, dropRef] = useDrop(() => ({
-    accept,
-    collect: monitor => ({
-      canDrop: monitor.canDrop(),
-      isOver: monitor.isOver(),
+}: BinType & { index: number; onDrop: (item: DropDataType) => void }) {
+  const [{ canDrop }, dropRef] = useDrop(
+    () => ({
+      accept,
+      collect: monitor => ({
+        canDrop: monitor.canDrop(),
+        isOver: monitor.isOver(),
+      }),
+      drop: onDrop,
     }),
-    drop: onDrop,
-  }))
+    [onDrop],
+  )
   return (
     <div
       ref={dropRef}
-      className='w-1/2 border border-blue-300 ring ring-offset-blue-400 bg-blue-100 p-4'
+      className='w-1/2 rounded border border-blue-300 ring ring-offset-blue-400 bg-blue-100 p-4'
     >
       <h2 className='font-bold text-2xl text-blue-800'>{label}</h2>
       {childrens.length > 0 && (
         <ul className='py-2 space-y-4'>
-          {childrens.map(({ id, label }) => (
-            <li
-              key={id}
-              className='p-2 border border-gray-500 ring ring-gray-500'
-            >
-              <h5 className='text-base font-bold text-gray-800'>{label}</h5>
-            </li>
+          {childrens.map(props => (
+            <SelectedCard key={props.id} containerIndex={index} {...props} />
           ))}
         </ul>
       )}
-      {canDrop && (
-        <div className='border-4 border-dashed border-gray-600 p-6 grid place-content-center'>
-          <h3 className='text-xl font-bold text-gray-700 text-center'>
-            + Drop here
-          </h3>
-        </div>
-      )}
+      {canDrop && <DropPlaceholderContainer />}
     </div>
   )
 }
@@ -176,7 +237,7 @@ function Card({ id, label, type }: CardsType) {
   return (
     <div
       ref={dragRef}
-      className={`w-full p-4 border bg-yellow-200 border-green-400 ring ring-gray-300 ${
+      className={`w-full p-4 rounded border bg-yellow-200 border-green-400 ring ring-gray-300 ${
         isDragging ? 'opacity-0' : 'opacity-100'
       }`}
     >
@@ -195,6 +256,7 @@ function Dropzones() {
           {...dropzoneProps}
           onDrop={item => onDrop(item, index)}
           key={dropzoneProps.id}
+          index={index}
         />
       ))}
     </>
@@ -202,13 +264,25 @@ function Dropzones() {
 }
 
 function Cards() {
-  const { cards } = useDataContext()
+  const { cards, onSelectDrop } = useDataContext()
+  const [{ canDrop }, dropRef] = useDrop(
+    () => ({
+      accept: 'selected_card',
+      drop: onSelectDrop,
+      collect: monitor => ({
+        canDrop: monitor.canDrop(),
+      }),
+    }),
+    [onSelectDrop],
+  )
+
   return (
-    <>
+    <div ref={dropRef} className='w-full space-y-4'>
       {cards.map(cardProps => (
         <Card {...cardProps} key={cardProps.id} />
       ))}
-    </>
+      {canDrop && <DropPlaceholderContainer />}
+    </div>
   )
 }
 
@@ -219,7 +293,7 @@ function App() {
         <div className='w-10/12 flex flex-col items-center justify-around'>
           <Dropzones />
         </div>
-        <div className='flex-1 p-4 flex flex-col items-center justify-start space-y-4'>
+        <div className='flex-1 p-4 flex flex-col items-center justify-start'>
           <Cards />
         </div>
       </div>
